4954a13799d5277b0861dad8f5e99cc673446dee
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-06-10 04:09:38 -0300

    Get votes

--- comovotadeputado/opendata/controller.py
+++ comovotadeputado/opendata/controller.py
@@ -21,11 +21,54 @@ def get_voted_propositions_dataframe(year):
 def _get_proposition(propostion_id):
     cache_key = 'proposition' + propostion_id    
     query_url = OpenDataConstants.PROPOSITION_ENDPOINT + "?IdProp=" + propostion_id
-    resp = fetch_data(cache_key, query_url)
+    resp = cache.http_fetch_data(cache_key, query_url)
     return resp
 
 def get_proposition(propostion_id):
     resp = _get_proposition(propostion_id)
-    resp_json = json.loads(json.dumps(xmltodict.parse(resp.content)))
-    propostion = resp_json['proposicao']
-    return None
+    resp_json = xml_to_json(resp.content)
+    proposition_json = resp_json['proposicao']
+    p_type = proposition_json['@tipo'].strip()
+    p_year = proposition_json['@ano'].strip()
+    p_number = proposition_json['@numero'].strip()      
+    proposition = Proposition(p_number, p_type, p_year)
+    return proposition
+
+def _get_polls(p_type, p_number, p_year):
+    cache_key = 'polls' + p_type + p_number + p_year
+    query_url = OpenDataConstants.POLLS_PROPOSITIONS_ENDPOINT + "?tipo=" + p_type + "&numero=" + p_number + "&ano=" + p_year
+    resp = cache.http_fetch_data(cache_key, query_url)
+    return resp
+
+def get_polls_dataframe(p_type, p_number, p_year, voted_date):
+
+    resp = _get_polls(p_type, p_number, p_year)    
+    if resp.status_code != HttpStatusCode.OK:
+        raise Exception(ErrorMessages.UNOBTAINABLE_DATA)
+        
+    resp_json = xml_to_json(resp.content)
+    polls = resp_json['proposicao']['Votacoes']['Votacao']
+    if '@Resumo' in polls: # has only one vote
+        polls = [polls]
+
+    polls_df = pd.DataFrame()
+    for poll in polls:
+
+        date = poll['@Data']
+        hour = poll['@Hora']
+                
+        #if date != voted_date: 
+            #continue
+            # date = 10/3/2018, voted_date = 10/03/2018
+
+        if polls_df.empty:
+            polls_df = pd.io.json.json_normalize(poll['votos']['Deputado'])
+            polls_df['@Data'] = date
+            polls_df['@Hora'] = hour
+        else:
+            new_votes = pd.io.json.json_normalize(poll['votos']['Deputado'])
+            new_votes['@Data'] = date
+            new_votes['@Hora'] = hour
+            polls_df = polls_df.append(new_votes)
+    
+    return polls_df    
