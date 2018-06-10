85705e860cd017ad681b87a20cb20d3f8e5a1b50
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-06-10 04:08:51 -0300

    Get voted propositions

--- comovotadeputado/opendata/controller.py
+++ comovotadeputado/opendata/controller.py
@@ -1,20 +1,22 @@
-import requests
-import json
-import xmltodict
-from comovotadeputado.cache import fetch_data
+import pandas as pd
+import comovotadeputado.cache as cache
 from comovotadeputado.opendata.constants import OpenDataConstants
+from comovotadeputado.errors.messages import ErrorMessages
+from comovotadeputado.constants.http import HttpStatusCode
+from comovotadeputado.utils.parser import xml_to_json
+from comovotadeputado.models.proposition import Proposition
 
-# def _get_proposition(propostion_id):
-    
-#     cache_key = 'proposition' + propostion_id    
-#     @cache.cache(cache_key, expire=3600)
-#     def fetch_data(propostion_id):
-#         query_url = OpenDataConstants.PROPOSITION_ENDPOINT + "?IdProp=" + propostion_id
-#         resp = requests.get(query_url)
-#         return resp
+def _get_voted_propositions(year):
+    cache_key = 'voted_propositions_' + year    
+    query_url = OpenDataConstants.VOTED_PROPOSITIONS_ENDPOINT + "?ano=" + year + "&tipo="
+    resp = cache.http_fetch_data(cache_key, query_url)
+    return resp
 
-#     results = fetch_data(propostion_id)
-#     return results
+def get_voted_propositions_dataframe(year):
+    resp = _get_voted_propositions(year)
+    resp_json = xml_to_json(resp.content)
+    propositions_df = pd.io.json.json_normalize(resp_json['proposicoes']['proposicao'])
+    return propositions_df
 
 def _get_proposition(propostion_id):
     cache_key = 'proposition' + propostion_id    
