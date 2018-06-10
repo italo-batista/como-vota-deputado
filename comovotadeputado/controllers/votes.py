5658c33144d071ab3cfadb02908e1e24901cae83
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-06-10 04:10:51 -0300

    Define votesCtrl

---
+++ comovotadeputado/controllers/votes.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+import comovotadeputado.opendata.controller as open_dt_ctrl
+from comovotadeputado.opendata.constants import OpenDataConstants
+from comovotadeputado.errors.messages import ErrorMessages
+from comovotadeputado.cache import cache
+from comovotadeputado.cache.constants import CacheConstants
+
+class VotesCtrl:
+
+    def get_all_votes_dataframe(self, year):
+        
+        cache_key = 'all_votes_dataframe' + year    
+        
+        @cache.cache(cache_key, expire=CacheConstants.EXPIRATION_TIME)
+        def _get_all_votes_dataframe(year):
+
+            propositions_df = open_dt_ctrl.get_voted_propositions_dataframe(year)        
+            all_votes_df = pd.DataFrame()
+            
+            for index, row in propositions_df.iterrows():
+        
+                cod_proposition = row['codProposicao']
+                voted_date = row['dataVotacao']
+                proposition = open_dt_ctrl.get_proposition(cod_proposition)
+               
+                try:
+                    polls_df = open_dt_ctrl.get_polls_dataframe(proposition.type, proposition.number, 
+                        proposition.year, voted_date)
+                except Exception as e:
+                    if e.args == ErrorMessages.UNOBTAINABLE_DATA:
+                        continue                
+        
+                if all_votes_df.empty:
+                    all_votes_df = polls_df
+                else:
+                    all_votes_df = all_votes_df.append(polls_df)
+        
+            return all_votes_df
+
+        return _get_all_votes_dataframe(year)        
+
+    def get_congressman_votes(self, congressman_id, year):                
+        all_votes_df = self.get_all_votes_dataframe(year)
+        congressman_votes_df = all_votes_df.loc[
+            (all_votes_df['@ideCadastro'] == congressman_id)]
+        return congressman_votes_df
