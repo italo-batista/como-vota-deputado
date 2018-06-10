cb67c98aafb3b5e04ad9eef56754c2a28a8c450a
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-06-09 23:09:02 -0300

    Fix setup import

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'comovotadeputado',
   packages = ['comovotadeputado'], 
   version = '0.1',
   description = 'To describe',
-  author = 'Ítalo Batista',
+  author = 'Italo Batista',
   author_email = 'italohmb@gmail.com',
   url = 'https://github.com/italo-batista/como-vota-deputado', 
   download_url = 'https://github.com/italo-batista/como-vota-deputado/archive/0.1.tar.gz',
