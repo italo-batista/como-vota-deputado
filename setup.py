f5707bf2e5ed357c1a838ffee347f576b3cdc7f2
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-08-14 00:19:37 -0300

    Adding requirements and test suite frw

--- setup.py
+++ setup.py
@@ -8,7 +8,20 @@ setup(
   author = 'Italo Batista',
   author_email = 'italohmb@gmail.com',
   url = 'https://github.com/italo-batista/como-vota-deputado', 
-  download_url = 'https://github.com/italo-batista/como-vota-deputado/archive/0.1.tar.gz',
+  download_url = 'https://github.com/italo-batista/como-vota-deputado/archive/1.0.tar.gz',
   keywords = ['opendata', 'politicians', 'br'], 
   classifiers = [],
+  install_requires=[
+    'beaker',
+    'pandas',
+    'requests',
+    'xmltodict',
+    'mock',
+    'nose',
+    'enum'
+  ],
+  test_suite='nose.collector',
+  tests_require=['nose'],
+  include_package_data=True,
+  zip_safe=False
 )
